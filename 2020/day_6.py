b5fd5c1f5503674f1c217bfab618e93ca642ea2a
sean <sean@cortirio.com>
2020-12-06 10:59:52 +0000

    Day 6 - part 2

--- 2020/day_6.py
+++ 2020/day_6.py
@@ -7,26 +7,28 @@ def parse_line(line):
 
 def parse_input(input, buffer_size):
     parsed_input = ['' for _ in range(buffer_size)]
-    print(parsed_input)
+    num_ppl = [0] * buffer_size
     entry_num = 0
     for line in input:
         if line == '':
             entry_num += 1
         else:
             parsed_input[entry_num] += line
-    return parsed_input
+            num_ppl[entry_num] += 1
+    return parsed_input, num_ppl
 
 
 if __name__ == '__main__':
     filename = 'day_6.txt'
     data = load_input(filename, parse_line)
 
-    for l in data:
-        print(l)
+    answers, num = parse_input(data, 1000)
 
-    data_parsed = parse_input(data, 1000)
-
-    counts = 0
-    for d in data_parsed:
-        counts += len(set(d))
-    print(counts)
+    sum_all_correct = 0
+    for a, n in zip(answers, num):
+        num_all_correct = 0
+        for c in set(a):
+            if a.count(c) == n:
+                num_all_correct += 1
+        sum_all_correct += num_all_correct
+    print(sum_all_correct)
