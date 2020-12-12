6f6f034f073aead1f042e45608b354abc5710e5d
sean <sean@cortirio.com>
2020-12-12 16:17:33 +0000

    Day 7 part 1
    Fixed bug in parsing into adjacency list

--- 2020/day_7.py
+++ 2020/day_7.py
@@ -4,35 +4,57 @@ import re
 
 def parse_line(line):
     line = line.strip('.\n')
-    # line = line.strip('.\n').strip(' bags').strip(' bag')
     line = re.sub(' bags$', '', line)
     line = re.sub(' bag$', '', line)
     a, b = line.split(' bags contain ')
     b_split = b.split(', ')
-    b_bit_stripped = [a.strip(' bag') for a in b_split]
-    b_stripped = [a.strip(' bags') for a in b_bit_stripped]
-
+    b_bit_stripped = [re.sub(' bag$', '', a) for a in b_split]
+    b_stripped = [re.sub(' bags$', '', a) for a in b_bit_stripped]
     # Assume numbers only ever 1 digit
     nums = [a[0] for a in b_stripped]
     colors = [a[2:] for a in b_stripped]
-
     return a, colors, nums
 
 
 def build_adjacency_dict(filename, parse_line):
-    # Note doesn't take into account no other bags correctly
+    # Note doesn't take into account no other bags
 
     adj_list = defaultdict(set)
     for line in open(filename).readlines():
         a, colors, nums = parse_line(line)
         for color in colors:
-            adj_list[color].add(a)
+            if color != ' other':
+                adj_list[color].add(a)
     return adj_list
 
 
+def dfs(key, adj_list, possible_bags):
+    possible_bags.add(key)
+
+    # if no more parent bags return
+    if key not in adj_list.keys():
+        return possible_bags
+    # Else do dfs on all parent bags
+    else:
+        for color in adj_list[key]:
+            dfs(color, adj_list, possible_bags)
+
+    return possible_bags
+
+
 if __name__ == '__main__':
-    filename = 'day_7_example_1.txt'
+    # filename = 'day_7_example_1.txt'
+    filename = 'day_7.txt'
+
     adj_list = build_adjacency_dict(filename, parse_line)
 
+    print('Adjacency list:')
     for a, b in adj_list.items():
         print(a, b)
+    print('')
+
+    possible_bags = set()
+    dfs('shiny gold', adj_list, possible_bags)
+
+    # print('Possible bags', possible_bags)
+    print('Answer', len(possible_bags) - 1)
