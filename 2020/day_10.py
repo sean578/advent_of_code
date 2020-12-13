24375bb5d0fcbc3c64bf6722e1ee6d2474b3bcd3
sean <sean@cortirio.com>
2020-12-13 20:28:44 +0000

    Day 10 - tidy up

--- 2020/day_10.py
+++ 2020/day_10.py
@@ -14,32 +14,22 @@ def parse_line(line):
 def create_adjacency_dict(data):
     adjacency_dict = defaultdict(list)
     for i, value in enumerate(data):
-        # print('i, value', i, value)
         j = 1
         while i + j < len(data) and data[i + j] <= value + 3:
-            # print('j, value', j, data[i + j])
             adjacency_dict[value].append(data[i + j])
             j += 1
     adjacency_dict[data[-1]] = []
     return adjacency_dict
 
 
-def dfs(adjacency_dict, start, count):
-    print(count)
-    if not adjacency_dict[start]:
-        return count + 1
-    else:
-        for node in adjacency_dict[start]:
-            count = dfs(adjacency_dict, node, count)
-
-    return count
-
-
 def dfs_dp(adjacency_dict, start, cache):
+    # If arrive at final mode then have found a path
     if not adjacency_dict[start]:
         return 1  # found a path
     else:
+        # Only do dfs if we haven't already stored the number of paths from this start node to the final node
         if start not in cache.keys():
+            # The number of paths to a node is the sum of number of paths to parent nodes
             cache[start] = sum(dfs_dp(adjacency_dict, c, cache) for c in adjacency_dict[start])
         return cache[start]
 
@@ -52,7 +42,6 @@ if __name__ == '__main__':
     data.append(data[-1] + 3)
     data.insert(0, 0)
 
-    print('Sorted input', data)
     diffs = []
     for x, y in zip(data, data[1:]):
         diffs.append(y-x)
@@ -68,14 +57,10 @@ if __name__ == '__main__':
     print('Answer', answer)
 
     print('Part 2')
-
     # Create a graph (adjacency dict)
     adjacency_dict = create_adjacency_dict(data)
-    print(adjacency_dict)
-    # for key, value in adjacency_dict.items():
-    #     print(key, value)
 
-    # Find number of paths between vertex 0 and 22
+    # Find number of paths between vertex 0 and final node
+    # Note final node is only element without a child in the adjacency list
     number_paths = dfs_dp(adjacency_dict, 0, {})
-    # number_paths = dfs(adjacency_dict, 0, 0)
     print('Answer part 2:', number_paths)
