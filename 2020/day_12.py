839b1e93c3834370158b556951e32fe08c4d52c2
sean <sean@cortirio.com>
2020-12-22 13:00:16 +0000

    Day 12 - part 2
    Number of 90 degree rotations (no need for angles)

--- 2020/day_12.py
+++ 2020/day_12.py
@@ -1,5 +1,3 @@
-
-
 def load_input(filename):
     commands, amounts = [], []
     for line in open(filename).readlines():
@@ -10,64 +8,64 @@ def load_input(filename):
     return commands, amounts
 
 
-def move(x_0, y_0, angle_0, command, amount):
-    angles = ('N', 'E', 'S', 'W')
+def move(x_0, y_0, x_ship_0, y_ship_0, command, amount):
+    """
+    Args:
+        x_0: x position of waypoint (rel. to ship)
+        y_0: y position of waypoint (rel. to ship)
+        x_ship_0: x postion of ship
+        y_ship_0: y position of ship
+    """
+
+    x_ship = x_ship_0
+    y_ship = y_ship_0
+    x = x_0
+    y = y_0
+
     if command == 'N':
         x = x_0
         y = y_0 + amount
-        angle = angle_0
     elif command == 'S':
         x = x_0
         y = y_0 - amount
-        angle = angle_0
     elif command == 'E':
         x = x_0 + amount
         y = y_0
-        angle = angle_0
     elif command == 'W':
         x = x_0 - amount
         y = y_0
-        angle = angle_0
     elif command == 'L':
-        x = x_0
-        y = y_0
-        angle = (angle_0 - amount // 90) % 4
+        for _ in range(amount // 90):
+            x = -y_0
+            y = x_0
+            x_0 = x
+            y_0 = y
     elif command == 'R':
-        x = x_0
-        y = y_0
-        angle = (angle_0 + amount // 90) % 4
+        for _ in range(amount // 90):
+            x = y_0
+            y = -x_0
+            x_0 = x
+            y_0 = y
     elif command == 'F':
-        if angle_0 == 0:
-            x = x_0
-            y = y_0 + amount
-        elif angle_0 == 1:
-            x = x_0 + amount
-            y = y_0
-        elif angle_0 == 2:
-            x = x_0
-            y = y_0 - amount
-        elif angle_0 == 3:
-            x = x_0 - amount
-            y = y_0
-        else:
-            'Incorrect angle encountered'
-        angle = angle_0
+        x_ship = x_ship_0 + x_0 * amount
+        y_ship = y_ship_0 + y_0 * amount
     else:
         print('Unknown command encountered')
 
-    return x, y, angle
+    return x, y, x_ship, y_ship
 
 
 if __name__ == '__main__':
     filename = 'day_12.txt'
     commands, amounts = load_input(filename)
 
-    x, y, angle = 0, 0, 1  # angle is an index to LUT
-    print('Initial position', x, y, angle)
+    x, y = 10, 1
+    x_ship, y_ship = 0, 0
+    print('Initial position', x, y, x_ship, y_ship)
+
     for c, a in zip(commands, amounts):
         print('Command', c, a)
-        x, y, angle = move(x, y, angle, c, a)
-        print('New position', x, y, angle)
+        x, y, x_ship, y_ship = move(x, y, x_ship, y_ship, c, a)
+        print('New position', x, y, x_ship, y_ship)
 
-    print(x, y, angle)
-    print('Answer part 1:', abs(x) + abs(y))
+    print('Answer part 2:', abs(x_ship) + abs(y_ship))
