191b47836284ab4b510e49508bed78c1bea1e754
sean <sean@cortirio.com>
2020-12-24 15:36:23 +0000

    Day 14 - part 2
    Use a dict instead of an array as not all addresses used (and way too much to fix max address in memory).

--- 2020/day_14.py
+++ 2020/day_14.py
@@ -1,3 +1,7 @@
+import itertools
+from copy import deepcopy
+
+
 def load_input(filename):
     data = None
     commands_raw, values = [], []
@@ -23,13 +27,18 @@ def load_input(filename):
 def get_mask_bits(mask):
     zeros = []
     ones = []
+    xs = []
 
     for i, x in enumerate(mask[::-1]):
         if x == '0':
             zeros.append(i)
         elif x == '1':
             ones.append(i)
-    return zeros, ones
+        elif x == 'X':
+            xs.append(i)
+        else:
+            print('Incorrect mask bit found')
+    return zeros, ones, xs
 
 
 def perform_mask(value, mask_zeros, mask_ones):
@@ -40,17 +49,38 @@ def perform_mask(value, mask_zeros, mask_ones):
     return value
 
 
+def perform_mask_part_2(mem_addr, mask_ones, mask_x):
+
+    # First set all bits to one where required
+    for i in mask_ones:
+        mem_addr = int(mem_addr) | (1 << i)
+
+    # Get all combinations of on/off for the floating bits
+    combinations = itertools.product([0, 1], repeat=len(mask_x))
+
+    addresses = []
+    for comb in combinations:
+        a = deepcopy(mem_addr)
+        for i, v in zip(mask_x, comb):
+            if v == 0:
+                a = a & ~(1 << i)
+            elif v == 1:
+                a = a | (1 << i)
+        addresses.append(a)
+    return addresses
+
+
 if __name__ == '__main__':
     filename = 'day_14.txt'
     commands, addresses, values = load_input(filename)
 
-    mask_zeros, mask_ones = None, None
-    mem = [0]*100000
+    mask_zeros, mask_ones, mask_xs = None, None, None
+    mem = {}
     for c, a, v in zip(commands, addresses, values):
         if c == 'mask':
-            mask_zeros, mask_ones = get_mask_bits(v)
+            mask_zeros, mask_ones, mask_xs = get_mask_bits(v)
         elif c == 'mem':
-            value = perform_mask(v, mask_zeros, mask_ones)
-            mem[int(a)] = value
-
-    print('Answer part 1:', sum(mem))
+            addresses = perform_mask_part_2(int(a), mask_ones, mask_xs)
+            for addr in addresses:
+                mem[addr] = int(v)
+    print('Answer part 2:', sum(mem.values()))
