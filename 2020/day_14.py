6f3cf67285d9e080cd61327e5c243af8e7679c6a
sean <sean@cortirio.com>
2020-12-24 12:27:35 +0000

    Day 14 - part 1

--- 2020/day_14.py
+++ 2020/day_14.py
@@ -1,18 +1,11 @@
 def load_input(filename):
     data = None
-    commands, values = [], []
+    commands_raw, values = [], []
     for line in open(filename).readlines():
         line = line.strip('\n')
         command, value = line.split(' = ')
-        commands.append(command)
+        commands_raw.append(command)
         values.append(value)
-
-    return commands, values
-
-
-if __name__ == '__main__':
-    filename = 'day_14_example_1.txt'
-    commands_raw, values = load_input(filename)
 
     commands = []
     addresses = []
@@ -24,5 +17,40 @@ if __name__ == '__main__':
         commands.append(c)
         addresses.append(a)
 
+    return commands, addresses, values
+
+
+def get_mask_bits(mask):
+    zeros = []
+    ones = []
+
+    for i, x in enumerate(mask[::-1]):
+        if x == '0':
+            zeros.append(i)
+        elif x == '1':
+            ones.append(i)
+    return zeros, ones
+
+
+def perform_mask(value, mask_zeros, mask_ones):
+    for i in mask_zeros:
+        value = int(value) & ~(1 << i)
+    for i in mask_ones:
+        value = int(value) | (1 << i)
+    return value
+
+
+if __name__ == '__main__':
+    filename = 'day_14.txt'
+    commands, addresses, values = load_input(filename)
+
+    mask_zeros, mask_ones = None, None
+    mem = [0]*100000
     for c, a, v in zip(commands, addresses, values):
-        print(c, a, v)
+        if c == 'mask':
+            mask_zeros, mask_ones = get_mask_bits(v)
+        elif c == 'mem':
+            value = perform_mask(v, mask_zeros, mask_ones)
+            mem[int(a)] = value
+
+    print('Answer part 1:', sum(mem))
