50c9a51b5f4e6a6ecb766848fafdb9899128c3e8
Sean <sean@cortirio.com>
2020-12-27 11:20:58 +0000

    Day 17 - Part 2

--- 2020/day_17.py
+++ 2020/day_17.py
@@ -17,18 +17,20 @@ def initialise_grid(data, num_cycles):
     grid_size = {
         'x': len(data[0]) + 2*num_cycles + 1,
         'y': len(data[1]) + 2*num_cycles + 1,
-        'z': len(data[2]) + 2*num_cycles + 1
+        'z': 1 + 2*num_cycles + 1,
+        'w': 1 + 2*num_cycles + 1
     }
 
     print('grid sizes:', grid_size)
 
-    grid = np.zeros((grid_size['x'], grid_size['y'], grid_size['z']))
+    grid = np.zeros((grid_size['x'], grid_size['y'], grid_size['z'], grid_size['w']))
 
     # Put the initial data in the middle of the array
     grid[
         (grid_size['x'] - len(data[1])) // 2 : (grid_size['x'] - len(data[1])) // 2 + len(data[0]),
         (grid_size['y'] - len(data[1])) // 2 : (grid_size['y'] - len(data[1])) // 2 + len(data[1]),
         grid_size['z'] // 2,
+        grid_size['w'] // 2
     ] = data
 
     return grid, grid_size
@@ -55,25 +57,27 @@ if __name__ == '__main__':
         print('cycle', cycle + 1)
         # use grid_old to check and then update grid
         grid_old = copy.deepcopy(grid)
-        deltas = list(itertools.product([-1, 0, 1], repeat=3))
-        deltas.remove((0, 0, 0))
+        deltas = list(itertools.product([-1, 0, 1], repeat=4))
+        deltas.remove((0, 0, 0, 0))
         for x in range(grid_shape['x']):
             for y in range(grid_shape['y']):
                 for z in range(grid_shape['z']):
-                    num_active = 0
-                    for delta in deltas:
-                        x_delta, y_delta, z_delta = x + delta[0], y + delta[1], z + delta[2]
-                        if 0 <= x_delta < grid_shape['x'] and \
-                           0 <= y_delta < grid_shape['y'] and \
-                           0 <= z_delta < grid_shape['z']:
-                            if grid_old[x_delta, y_delta, z_delta]:
-                                num_active += 1
-                    if grid_old[x, y, z]:
-                        if num_active < 2 or num_active > 3:
-                            grid[x, y, z] = 0
-                    else:
-                        if num_active == 3:
-                            grid[x, y, z] = 1
+                    for w in range(grid_shape['w']):
+                        num_active = 0
+                        for delta in deltas:
+                            x_delta, y_delta, z_delta, w_delta = x + delta[0], y + delta[1], z + delta[2], w + delta[3]
+                            if 0 <= x_delta < grid_shape['x'] and \
+                               0 <= y_delta < grid_shape['y'] and \
+                               0 <= z_delta < grid_shape['z'] and \
+                               0 <= w_delta < grid_shape['w']:
+                                if grid_old[x_delta, y_delta, z_delta, w_delta]:
+                                    num_active += 1
+                        if grid_old[x, y, z, w]:
+                            if num_active < 2 or num_active > 3:
+                                grid[x, y, z, w] = 0
+                        else:
+                            if num_active == 3:
+                                grid[x, y, z, w] = 1
 
         print_grid(grid)
     print('Answer:', np.count_nonzero(grid))
