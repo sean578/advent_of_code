ff81061d9b7399f181e0504d188063ad3646cb0e
Sean <sean@cortirio.com>
2021-01-02 10:50:46 +0000

    Day 23 - WIP

---
+++ 2020/day_23.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    initial = [int(i) for i in '952316487']
+    initial_1 = [int(i) for i in '389125467']
+
+    num_moves = 10
+    current_index = 0
+    destination_index = None
+
+    pick_up = initial_1
