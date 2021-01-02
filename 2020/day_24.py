cdad245a6b328328c6cd9d479893c9585e41bf63
sean <sean@cortirio.com>
2021-01-02 20:02:15 +0000

    day 24 - part 2

--- 2020/day_24.py
+++ 2020/day_24.py
@@ -1,3 +1,5 @@
+import copy
+
 def load_input(filename):
     all_instructions = []
     for line in open(filename).readlines():
@@ -30,22 +32,27 @@ def parse_instruction(flips):
             instructions.append('w')
             index += 1
         else:
-            print('Input error', ex_1)
+            print('Input error', flips)
 
     return instructions
+
+
+def num_black_tiles(tile_colors):
+    num_black = 0
+    for color in tile_colors.values():
+        if color:
+            num_black += 1
+    return num_black
+
+
+def print_tiles(tile_colors):
+    for tile, color in tile_colors.items():
+        print(tile, color)
 
 
 if __name__ == '__main__':
     filename = 'day_24.txt'
     all_instructions = load_input(filename)
-
-    # ex_1 = 'esew'
-    # ex_2 = 'nwwswee'
-    # flips = ex_2
-    # instructions = parse_instruction(flips)
-    # print('Instructions', instructions)
-
-
     reference_tile = (0, 0)
 
     moves = {
@@ -71,9 +78,39 @@ if __name__ == '__main__':
         else:
             tile_colors[key] = True  # True = black
 
-    num_black = 0
-    for color in tile_colors.values():
-        if color:
-            num_black += 1
+    num_black_tiles(tile_colors)
 
-    print('Number black:', num_black)
+    # Part 2
+
+    for i in range(100):
+
+        # Extend dictionary to include tiles next to 1 black tile
+        temp = copy.deepcopy(tile_colors)
+        for tile in tile_colors.keys():
+            if tile_colors[tile]:
+                for x, y in moves.values():
+                    if (x + tile[0], y + tile[1]) not in tile_colors.keys():
+                        temp[(x + tile[0], y + tile[1])] = False
+        tile_colors = temp
+
+        # Go through and do the flipping
+        temp = copy.deepcopy(tile_colors)
+        for tile in tile_colors.keys():
+            count_black = 0
+            for x, y in moves.values():
+                if (x + tile[0], y + tile[1]) in tile_colors.keys():
+                    if tile_colors[(x + tile[0], y + tile[1])]:
+                        count_black += 1
+            # black
+            if tile_colors[tile]:
+                if (count_black == 0) or (count_black > 2):
+                    temp[tile] = False
+            # white
+            else:
+                if count_black == 2:
+                    temp[tile] = True
+
+        tile_colors = temp
+
+    num_black_tiles = num_black_tiles(tile_colors)
+    print('Answer part 2:', num_black_tiles)
