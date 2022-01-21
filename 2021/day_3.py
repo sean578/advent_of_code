1f0f5bd4713f4635b03ddfba4cc8077a276d3f85
Sean <sean@cortirio.com>
2022-01-21 09:46:00 +0000

    2021 - day 3 - Refactor to use filter

--- 2021/day_3.py
+++ 2021/day_3.py
@@ -9,23 +9,34 @@ def get_most_least_common(nums, index, most_common=True):
             sum -= 1
         else:
             sum += 1
-    if sum > 0:
+
+    if sum >= 0:
         if most_common:
             mc = '1'
         else:
             mc = '0'
-    elif sum < 0:
-        if most_common:
-            mc = '0'
-        else:
-            mc = '1'
     else:
         if most_common:
-            mc = '1'
-        else:
             mc = '0'
+        else:
+            mc = '1'
 
     return mc
+
+
+def find_rating(nums, num_digits, most_common=True):
+
+    for i in range(num_digits):
+        # Get most common in each col of words left
+        mc = get_most_least_common(nums=nums, index=i, most_common=most_common)
+
+        # Filter the nums
+        if len(nums) == 1:
+           break
+        else:
+            nums = list(filter(lambda x: x[i] == mc, nums))
+
+    return nums[0]
 
 
 if __name__ == '__main__':
@@ -37,46 +48,7 @@ if __name__ == '__main__':
     nums_o2 = copy.deepcopy(nums)
     nums_co2 = copy.deepcopy(nums)
 
+    nums_o2 = find_rating(nums_o2, num_digits, most_common=True)
+    nums_co2 = find_rating(nums_co2, num_digits, most_common=False)
 
-    for i in range(num_digits):
-
-        print('len', len(nums_o2))
-        print(nums_o2)
-
-        # Get most common in each col of words left
-        mc = get_most_least_common(nums=nums_o2, index=i, most_common=True)
-
-        # Filter the nums
-        ok_o2 = []
-        if len(nums_o2) == 1:
-            print('found o2')
-            break
-        else:
-            for l in nums_o2:
-                if l[i] == mc:
-                    ok_o2.append(l)
-            nums_o2 = copy.deepcopy(ok_o2)
-
-
-    for i in range(num_digits):
-
-        # Get least common in each col of words left
-        lc = get_most_least_common(nums=nums_co2, index=i, most_common=False)
-
-        # Filter the nums
-        ok_co2 = []
-
-        if len(nums_co2) == 1:
-            print('found co2')
-            break
-        else:
-            for l in nums_co2:
-                if l[i] == lc:
-                    ok_co2.append(l)
-            nums_co2 = copy.deepcopy(ok_co2)
-
-
-
-    print(nums_o2)
-    print(nums_co2)
-    print(int(nums_o2[0], 2) * int(nums_co2[0], 2))
+    print(int(nums_o2, 2) * int(nums_co2, 2))
