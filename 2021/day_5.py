22f8c0a03451e8da57fa794fac25e27a62084316
Sean <sean@cortirio.com>
2022-01-23 14:07:05 +0000

    2021 - day 5 - part 2

--- 2021/day_5.py
+++ 2021/day_5.py
@@ -19,21 +19,21 @@ def get_coord_list(coord):
     x2 = coord[1][0]
     y2 = coord[1][1]
 
-    if y2 == y1:
-        if x2 >= x1:
-            x = list(range(x1, x2 + 1))
-            y = len(x) * [y1]
-        else:
-            x = list(range(x1, x2 - 1, -1))
-            y = len(x) * [y1]
+    if x2 > x1:
+        x = list(range(x1, x2 + 1))
+    else:
+        x = list(range(x1, x2 - 1, -1))
 
+    if y2 > y1:
+        y = list(range(y1, y2 + 1))
+    else:
+        y = list(range(y1, y2 - 1, -1))
+
+    # If x1 = x2 or y1 = y2 then need to extend so have correct number elements
     if x2 == x1:
-        if y2 >= y1:
-            y = list(range(y1, y2 + 1))
-            x = len(y) * [x1]
-        else:
-            y = list(range(y1, y2 - 1, -1))
-            x = len(y) * [x1]
+        x = len(y) * x
+    if y2 == y1:
+        y = len(x) * y
 
     return x, y
 
@@ -41,18 +41,19 @@ def get_coord_list(coord):
 if __name__ == '__main__':
 
     data = read_data('day_5.txt')
+    grid_size = 1000
 
     # For part 1, filter out diagonal lines
-    data_hor = list(filter(lambda d: (d[0][0] == d[1][0]) or (d[0][1] == d[1][1]), data))
+    # data_hor = list(filter(lambda d: (d[0][0] == d[1][0]) or (d[0][1] == d[1][1]), data))
 
     all_coords_x = []
     all_coords_y = []
-    for coord in data_hor:
+    for coord in data:
         x, y = get_coord_list(coord)
         all_coords_x += x
         all_coords_y += y
 
-    counts = np.zeros((1000, 1000))
+    counts = np.zeros((grid_size, grid_size))
     for x, y in zip(all_coords_x, all_coords_y):
         counts[x, y] += 1
 
