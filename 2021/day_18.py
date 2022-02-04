fdebe85129e8cb2766f833155a0deb54e66c7b3f
Sean <sean@cortirio.com>
2022-02-04 22:24:52 +0000

    2021 - day 18 - part 2
    Slow. But works.

--- 2021/day_18.py
+++ 2021/day_18.py
@@ -35,7 +35,7 @@ def explode(depth_list):
 
     exploded = False
     for i, leaf in enumerate(depth_list):
-        if leaf[0] == 5:
+        if leaf[0] >= 5:
             exploded = True
             # If there is a number to the left
             if i > 0:
@@ -73,60 +73,85 @@ def split(depth_list):
 
 def add(depth_list_a, depth_list_b):
 
+    depth_list_a_new = copy.deepcopy(depth_list_a)
+    depth_list_b_new = copy.deepcopy(depth_list_b)
+
     # All values go one level deeper
     for i, val in enumerate(depth_list_a):
-        depth_list_a[i][0] += 1
+        depth_list_a_new[i][0] += 1
 
     for i, val in enumerate(depth_list_b):
-        depth_list_b[i][0] += 1
+        depth_list_b_new[i][0] += 1
 
-    return depth_list_a + depth_list_b
+    return depth_list_a_new + depth_list_b_new
 
 
 def find_mag(depth_list):
-    while(len(depth_list) > 1):
-        # Find max depth in list
-        max_depth = max([i[0] for i in depth_list])
-        depth_list_new = []
-        # For each pair at max depth find mag and reduce depth by 1 (2 numbers -> 1)
-        i = 0
-        while i < len(depth_list):
-            if depth_list[i][0] == max_depth:
-                a = depth_list[i][1]
-                b = depth_list[i+1][1]
-                mag = 3*a + 2*b
-                depth_list_new.append([depth_list[i][0] - 1, mag])
-            else:
-                depth_list_new.append(depth_list[i])
-                depth_list_new.append(depth_list[i+1])
-            i += 2
+    depth_list_new = copy.deepcopy(depth_list)
 
-        depth_list = copy.deepcopy(depth_list_new)
-    return depth_list[0][1]
+    while depth_list_new[0][0] != 0:
+        i = 0
+        while depth_list_new[i][0] != depth_list_new[i+1][0]:
+            i += 1
+        level, a = depth_list_new.pop(i)
+        _, b = depth_list_new.pop(i)
+        mag = 3*a + 2*b
+        depth_list_new.insert(i, [level - 1, mag])
+
+    return depth_list_new[0][1]
 
 
 if __name__ == '__main__':
     data = read_data('day_18.txt')
     data = list(map(create_depth_list, data))
 
-    depth_list = data[0]
-    for line in data[1:]:
+    part_1 = False
 
-        # add the numbers
-        depth_list = add(depth_list, line)
+    # Part 1 : Add the numbers up in sequence...
+    if part_1:
+        depth_list = data[0]
+        for line in data[1:]:
 
-        # do the reduction
-        while True:
-            exploded, depth_list = explode(depth_list)
-            if exploded:
-                continue
-            splitted, depth_list = split(depth_list)
-            if splitted:
-                continue
-            break
+            # add the numbers
+            depth_list = add(depth_list, line)
 
-    print(depth_list)
-    print([[i[1]] for i in depth_list])
+            # do the reduction
+            while True:
+                exploded, depth_list = explode(depth_list)
+                if exploded:
+                    continue
+                splitted, depth_list = split(depth_list)
+                if splitted:
+                    continue
+                break
 
-    mag = find_mag(depth_list)
-    print('mag', mag)
+
+        mag = find_mag(depth_list)
+        print('mag', mag)
+
+    # Part 2 : Add all combinations of numbers
+
+    n = len(data)
+    biggest_mag = 0
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+
+                # add the numbers
+                depth_list = add(data[i], data[j])
+
+                # do the reduction
+                while True:
+                    exploded, depth_list = explode(depth_list)
+                    if exploded:
+                        continue
+                    splitted, depth_list = split(depth_list)
+                    if splitted:
+                        continue
+                    break
+
+                mag = find_mag(depth_list)
+                if mag > biggest_mag:
+                    biggest_mag = mag
+
+    print('Biggest mag', biggest_mag)
