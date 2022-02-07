756723739b67d3d3de00039e62b6c515ecc594aa
Sean <sean@cortirio.com>
2022-02-07 10:04:46 +0000

    WIP: 2021 - day 19
    Refactor to speed up

--- 2021/day_19.py
+++ 2021/day_19.py
@@ -6,91 +6,76 @@ import copy
 def read_data(filename):
     lines = [line.strip() for line in open(filename).readlines()]
 
-    scans = defaultdict(list)
+    scans = defaultdict(None)
 
     scanner_number = -1
+    first = False
     for line in lines:
         if line[:3] == '---':
             scanner_number += 1
+            first = True
         elif line == '':
             pass
         else:
-            scans[scanner_number].append(np.array([int(i) for i in line.split(',')], np.int16))
+            beacon_as_list = [int(i) for i in line.split(',')]
+            if first:
+                first = False
+                scans[scanner_number] = np.array(beacon_as_list, np.int16)
+            else:
+                scans[scanner_number] = np.vstack((scans[scanner_number], np.array(beacon_as_list, np.int16)))
 
     return scans
 
 
-def roll(v, dir):
+def rotate(v, dir):
     if dir == 'R':
-        return v[0], v[2], -v[1]
+        return v @ np.array([[1, 0, 0], [0, 0, 1], [0, -1, 0]])
     elif dir == 'T':
-        return -v[1], v[0], v[2]
+        return v @ np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
     else:
-        print('Incorrect direction')
+        print('Incorrect direction:', dir)
 
 
-def rotate_vector(vector, num_rotations):
-
-    v = copy.deepcopy(vector)
+def rotate_vectors(beacons, num_rotations):
 
     a = 'RTTTRTTTRTTT'
     turn_around = 'RTR'
 
-    if num_rotations == 0:
-        return v
-    elif num_rotations <= 12:
-        for i in range(num_rotations):
-            v = roll(v, a[i])
+    if num_rotations == -1:
+        return beacons
+    elif num_rotations < 12:
+        beacons = rotate(beacons, a[num_rotations])
+    elif num_rotations == 12:
+        for i in turn_around:
+            beacons = rotate(beacons, i)
+        beacons = rotate(beacons, a[num_rotations % 12])
     else:
-        for r in a:
-            v = roll(v, r)
-        for r in turn_around:
-            v = roll(v, r)
-        for i in range(num_rotations % 12):
-            v = roll(v, a[i])
+        beacons = rotate(beacons, a[num_rotations % 12])
 
-    return v
+    return beacons
 
 
 if __name__ == '__main__':
     scans = read_data('day_19_test.txt')
-    print('Input data:')
-    for scanner, beacons in scans.items():
-        print(scanner, beacons)
-
-    # For scanner 2:
-        # Set absolute position such that a beacon line up
-        # Transform all coords into absolute and check overlap - are there 12 beacons
-        # If 12 beacons found, calc absolute position of scanner 2. Else...
-        # Do rotation of all coords and try again
-        # If out of rotations finish and move on to next scanner
 
     # for scanner 1..n while all overlaps not found
-    for num_rotations in range(24):
 
-        for i, beacon_abs in enumerate(scans[0]):
-            for j, beacon_rel in enumerate(scans[1]):
-                # Rotate beacon
-                beacon_rel = rotate_vector(beacon_rel, num_rotations)
+    for i, beacon_abs in enumerate(scans[0]):
+        beacons_rotated = copy.deepcopy(scans[1])
+        for num_rotations in range(24):
+            beacons_rotated = rotate_vectors(beacons_rotated, num_rotations)
+            x = set([tuple(i) for i in scans[0]])
+            for j, beacon_rel in enumerate(beacons_rotated):
                 # Find transformation such that the two beacons overlap
                 translation = beacon_abs - beacon_rel
-                # print('Translation', translation)
                 # Transform into abs coords
-                beacon_in_abs = [tuple(rotate_vector(beacon, num_rotations) + translation) for beacon in scans[1]]
-                orig = [tuple(b) for b in scans[0]]
-                # print('Beacon in abs', beacon_in_abs)
-                num_overlap = len(set(orig).intersection(set(beacon_in_abs)))
-                if num_overlap > 1:
+                beacon_in_abs = beacons_rotated + translation
+                # TODO: Find a numpy way to do the number of common rows to speed up...
+                y = set([tuple(i) for i in beacon_in_abs])
+                num_overlap = len(x.intersection(y))
+                if num_overlap == 12:
                     print('Translation', translation)
                     print(num_rotations, i, j, num_overlap)
 
 
     # Keep going checking against scanners whose absolute position is known until all scanner positions known
-
-    # Complexity:
-    # n^2 where n = number of scanners
-    # 24 rotations
-    # 25*25 = 625 beacons to overlap
-    # 25 checks for overlap
-    # 900 * 24 * 625 * 25 = 300e6 calcs
-
