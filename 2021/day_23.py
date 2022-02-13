6a7ba21daa9456f855e8a245d0d221abe172c7f1
Sean <sean@cortirio.com>
2022-02-13 22:17:12 +0000

    2021 - day 23 - part 2
    Needs a tidy!

--- 2021/day_23.py
+++ 2021/day_23.py
@@ -14,7 +14,7 @@ def create_state(state_dict, state_order):
 
 
 def print_state(state, state_order):
-    ymax = 5
+    ymax = 7
     xmax = 13
 
     grid = [['#' for _ in range(xmax)] for _ in range(ymax)]
@@ -22,8 +22,8 @@ def print_state(state, state_order):
     for x in range(1, xmax-1):
         grid[1][x] = '.'
     for x in (3, 5, 7, 9):
-        grid[2][x] = '.'
-        grid[3][x] = '.'
+        for y in range(2, 6):
+            grid[y][x] = '.'
     for i, s in enumerate(state):
         for e in s:
 
@@ -35,17 +35,32 @@ def print_state(state, state_order):
 
 
 def get_blocks(state):
-    # Get a list of x coords that are blocking
-    blocks_y1 = set()
-    blocks_y2 = set()
-    for color in state:
+    # Get a list of amphipods which are blocking the hall and homes
+    blocks_home = {
+        2: 0,
+        4: 0,
+        6: 0,
+        8: 0
+    }
+    blocks_home_types = {
+        2: set(),
+        4: set(),
+        6: set(),
+        8: set()
+    }
+
+    # [2, 4, 6, 8]
+
+    blocks_hallway = set()
+    for i, color in enumerate(state):
         for a in color:
             x, y = a
-            if y == 2:
-                blocks_y2.add(x)
-            elif y == 1:
-                blocks_y1.add(x)
-    return blocks_y1, blocks_y2
+            if y == 4:
+                blocks_hallway.add(x)
+            else:
+                blocks_home[x] += 1
+                blocks_home_types[x].add(2*i + 2)
+    return blocks_hallway, blocks_home, blocks_home_types
 
 
 def find_blocking_pos(hallway_pos, amphipod):
@@ -80,7 +95,8 @@ def create_new_state(old_state, lc, i, j, new_coords):
 
 def get_neighbours(state, state_order, allowed_room_x, energy_lookup):
     neighbours = {}  # new state: (x, y, delta energy) tuples
-    home_blocks, hallway_pos = get_blocks(state)
+    # Get list of number blocking
+    blocks_hallway, blocks_home, blocks_home_types = get_blocks(state)
 
     # For each amphipod get its neighbour states
     for i, color in enumerate(state):
@@ -88,49 +104,30 @@ def get_neighbours(state, state_order, allowed_room_x, energy_lookup):
         home_x = allowed_room_x[state_order[i]]
         lc = list(color)
 
-        # Find if home of color is occupied
-        y0_occ = False
-        y1_occ = False
-        for j, c in enumerate(state):
-            for b in c:
-                bx, by = b
-                if by == 0 and bx == home_x:
-                    y0_occ = True
-                    if state_order[j] != state_order[i]:
-                        y1_occ = True
-                if by == 1 and bx == home_x:
-                    y1_occ = True
-
         # Find the possible neighbour states for each occurance of color
         for k, a in enumerate(lc):
             x, y = a
-            lb, sb = find_blocking_pos(hallway_pos, a)
 
-            if y == 1 or (y == 0 and x not in home_blocks):
+            lb, sb = find_blocking_pos(blocks_hallway, a)
+
+            # If in the hallway, can move to home?
+            if y == 4:
+                # IN HALLWAY -> MOVE TO HOME
+                #todo: simplify this!
+                if blocks_home[home_x] <= 3:
+                    if (blocks_home[home_x] == 0) or ((len(blocks_home_types[home_x]) == 1) and (home_x in blocks_home_types[home_x])):
+                        if home_x in range(lb+1, sb):
+                            ns = create_new_state(state, lc, i, k, (home_x, blocks_home[home_x]))
+                            neighbours[ns] = e * number_moves(x, y, home_x, blocks_home[home_x])
+            # If in a borrow, if the highest block can move
+            elif blocks_home[x] - 1 <= y:
+                # IN HOME -> MOVE TO HALLWAY
                 for xi in range(lb+1, sb):
                     if xi in (0, 1, 3, 5, 7, 9, 10):
-                        ns = create_new_state(state, lc, i, k, (xi, 2))
-                        neighbours[ns] = e * number_moves(x, y, xi, 2)
-            elif y == 2:
-                if not (y0_occ and y1_occ):
-                    if home_x in range(lb+1, sb):
-                        if y0_occ:
-                            ns = create_new_state(state, lc, i, k, (home_x, 1))
-                            neighbours[ns] = e * number_moves(x, y, home_x, 1)
-                        else:
-                            ns = create_new_state(state, lc, i, k, (home_x, 0))
-                            neighbours[ns] = e * number_moves(x, y, home_x, 0)
+                        ns = create_new_state(state, lc, i, k, (xi, 4))
+                        neighbours[ns] = e * number_moves(x, y, xi, 4)
+
     return neighbours
-
-
-def make_move(state, neighbours, index, move_number):
-    # Return updated state of new position after move made
-
-    state[index].x = neighbours[index][move_number][0]
-    state[index].y = neighbours[index][move_number][1]
-    energy = neighbours[index][move_number][2]
-
-    return state, energy
 
 
 def initialise(initial_state):
@@ -171,7 +168,7 @@ def dijkstra(queue, visited, energy_dict, parent_dict, allowed_room_x, energy_lo
             # If new energy less than results energy then update results & push to queue
             new_energy = energy + energy_delta
             if neighbour in energy_dict:
-                old_energy = energy_dict[neighbour]  # todo need to check if state exists
+                old_energy = energy_dict[neighbour]
             else:
                 old_energy = math.inf
             if new_energy < old_energy:
@@ -180,7 +177,7 @@ def dijkstra(queue, visited, energy_dict, parent_dict, allowed_room_x, energy_lo
                 energy_dict[neighbour] = new_energy
 
             if len(energy_dict) % 10000 == 0:
-                print('Number of new states:', len(energy_dict))
+                print('Number of new states:', len(energy_dict), 'Length of queue:', len(queue))
                 if final_state in energy_dict:
                     print('Final state energy:', energy_dict[final_state])
 
@@ -220,24 +217,24 @@ if __name__ == '__main__':
     # initial & final states
     # ((x1, y1), (x2, y2))
     # initial_state_dict = {
-    #     'A': ((2, 0), (8, 0)),
-    #     'B': ((2, 1), (6, 1)),
-    #     'C': ((4, 1), (6, 0)),
-    #     'D': ((4, 0), (8, 1)),
+    #     'A': ((2, 0), (8, 0), (6, 1), (8, 2)),
+    #     'B': ((2, 3), (6, 3), (4, 1), (6, 2)),
+    #     'C': ((4, 3), (6, 0), (4, 2), (8, 1)),
+    #     'D': ((4, 0), (8, 3), (2, 1), (2, 2)),
     # }
 
     initial_state_dict = {
-        'A': ((4, 1), (8, 0)),
-        'B': ((2, 0), (6, 0)),
-        'C': ((4, 0), (8, 1)),
-        'D': ((2, 1), (6, 1)),
+        'A': ((4, 3), (8, 0), (6, 1), (8, 2)),
+        'B': ((2, 0), (6, 0), (4, 1), (6, 2)),
+        'C': ((4, 0), (8, 3), (4, 2), (8, 1)),
+        'D': ((2, 3), (6, 3), (2, 1), (2, 2)),
     }
 
     final_state_dict = {
-        'A': ((2, 0), (2, 1)),
-        'B': ((4, 0), (4, 1)),
-        'C': ((6, 0), (6, 1)),
-        'D': ((8, 0), (8, 1)),
+        'A': ((2, 0), (2, 1), (2, 2), (2, 3)),
+        'B': ((4, 0), (4, 1), (4, 2), (4, 3)),
+        'C': ((6, 0), (6, 1), (6, 2), (6, 3)),
+        'D': ((8, 0), (8, 1), (8, 2), (8, 3)),
     }
 
     state = create_state(initial_state_dict, state_order)
@@ -245,9 +242,28 @@ if __name__ == '__main__':
     print(state)
     print_state(state, state_order)
     final_state = create_state(final_state_dict, state_order)
-
+    #
+    # states = [
+    #     (frozenset({(2, 0), (6, 1), (8, 0), (8, 2)}), frozenset({(6, 3), (6, 2), (4, 1), (2, 3)}), frozenset({(8, 1), (4, 2), (6, 0), (4, 3)}), frozenset({(4, 0), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (6, 1), (8, 0), (0, 4)}), frozenset({(6, 3), (6, 2), (4, 1), (2, 3)}), frozenset({(8, 1), (4, 2), (6, 0), (4, 3)}), frozenset({(4, 0), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (6, 1), (8, 0), (0, 4)}), frozenset({(6, 2), (4, 1), (9, 4), (2, 3)}), frozenset({(8, 1), (4, 2), (6, 0), (4, 3)}), frozenset({(4, 0), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (6, 1), (8, 0), (0, 4)}), frozenset({(7, 4), (4, 1), (9, 4), (2, 3)}), frozenset({(8, 1), (4, 2), (6, 0), (4, 3)}), frozenset({(4, 0), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (8, 0), (1, 4), (0, 4)}), frozenset({(7, 4), (4, 1), (9, 4), (2, 3)}), frozenset({(8, 1), (4, 2), (6, 0), (4, 3)}), frozenset({(4, 0), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (8, 0), (1, 4), (0, 4)}), frozenset({(7, 4), (4, 1), (9, 4), (2, 3)}), frozenset({(8, 1), (4, 2), (5, 4), (6, 0)}), frozenset({(4, 0), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (8, 0), (1, 4), (0, 4)}), frozenset({(7, 4), (4, 1), (9, 4), (2, 3)}), frozenset({(8, 1), (4, 2), (6, 1), (6, 0)}), frozenset({(4, 0), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (8, 0), (1, 4), (0, 4)}), frozenset({(7, 4), (4, 1), (9, 4), (2, 3)}), frozenset({(8, 1), (5, 4), (6, 0), (6, 1)}), frozenset({(4, 0), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (8, 0), (1, 4), (0, 4)}), frozenset({(7, 4), (4, 1), (9, 4), (2, 3)}), frozenset({(8, 1), (6, 1), (6, 2), (6, 0)}), frozenset({(4, 0), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (8, 0), (1, 4), (0, 4)}), frozenset({(7, 4), (5, 4), (2, 3), (9, 4)}), frozenset({(8, 1), (6, 1), (6, 2), (6, 0)}), frozenset({(4, 0), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (8, 0), (1, 4), (0, 4)}), frozenset({(7, 4), (5, 4), (2, 3), (9, 4)}), frozenset({(8, 1), (6, 1), (6, 2), (6, 0)}), frozenset({(3, 4), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (8, 0), (1, 4), (0, 4)}), frozenset({(7, 4), (2, 3), (9, 4), (4, 0)}), frozenset({(8, 1), (6, 1), (6, 2), (6, 0)}), frozenset({(3, 4), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (8, 0), (1, 4), (0, 4)}), frozenset({(4, 0), (4, 1), (9, 4), (2, 3)}), frozenset({(8, 1), (6, 1), (6, 2), (6, 0)}), frozenset({(3, 4), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (8, 0), (1, 4), (0, 4)}), frozenset({(4, 2), (4, 1), (2, 3), (4, 0)}), frozenset({(8, 1), (6, 1), (6, 2), (6, 0)}), frozenset({(3, 4), (10, 4), (2, 1), (2, 2)})),
+    #     (frozenset({(2, 0), (8, 0), (1, 4), (0, 4)}), frozenset({(2, 3), (4, 2), (4, 1), (4, 0)}), frozenset({(7, 4), (6, 1), (6, 2), (6, 0)}), frozenset({(3, 4), (10, 4), (2, 1), (2, 2)}))
+    # ]
+    #
     # print('INITIAL STATE:')
-    # state = frozenset({(2, 0), (7, 2)}), frozenset({(6, 1), (2, 1)}), frozenset({(4, 1), (6, 0)}), frozenset({(9, 2), (4, 0)})
+    # # state = states[-1]
+    # state = (frozenset({(5, 4), (6, 1), (4, 3), (0, 4)}), frozenset({(2, 0), (6, 2), (4, 1), (6, 0)}), frozenset({(4, 2), (10, 4), (9, 4), (4, 0)}), frozenset({(6, 3), (7, 4), (8, 0), (8, 1)}))
     # print(state)
     # print_state(state, state_order)
     #
