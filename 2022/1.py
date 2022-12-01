d88b7f888f4b3c5284c7fe3ba72983462fe24545
Sean Donnellan <sean.donnellan@blueprism.com>
2022-12-01 14:11:10 +0000

    Answer part 2

--- 2022/1.py
+++ 2022/1.py
@@ -3,6 +3,8 @@
 
 if __name__ == '__main__':
 
+
+    # part 1
     lines = [line.strip() for line in open("1_input.txt", 'r').readlines()]
 
     total = 0
@@ -16,3 +18,17 @@ if __name__ == '__main__':
             total += int(line)
 
     print(biggest)
+
+    # part 2
+
+    cals = []
+    total = 0
+    for line in lines:
+        if line == '':
+            cals.append(total)
+            total = 0
+        else:
+            total += int(line)
+
+    cals.sort(reverse=True)
+    print(sum(cals[:3]))
