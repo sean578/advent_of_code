730cb939d2453d3913483b7fd7a6a0fe20e59ddf
Sean Donnellan <sean.donnellan@blueprism.com>
2022-12-24 22:33:38 +0000

    Answer part 2

--- 2022/6.py
+++ 2022/6.py
@@ -4,12 +4,14 @@
 if __name__ == '__main__':
     signal = open("6_input.txt").readline().strip()
 
+    LEN = 14
+
     buffer = []
     for i, s in enumerate(signal):
-        if i <= 3:
+        if i <= LEN - 1:
             buffer.append(s)
         else:
-            assert len(buffer) == 4, buffer
+            assert len(buffer) == LEN, buffer
             if len(set(buffer)) == len(buffer):
                 print("Found", i)
                 break
