a3e8bd9ae6764e224c2efa40a16d622cc61cdc4d
Sean Donnellan <sean.donnellan@blueprism.com>
2022-12-27 23:35:43 +0000

    Part 2

--- 2022/8.py
+++ 2022/8.py
@@ -1,11 +1,8 @@
 import numpy as np
 
 
-if __name__ == '__main__':
-    a = [[int(l) for l in line.strip()] for line in open("8_input.txt").readlines()]
-    b = np.array(a, dtype=int)
+def part1(b):
 
-    c = np.zeros_like(b)
     h, w = b.shape
 
     num_visible = 0
@@ -27,4 +24,46 @@ if __name__ == '__main__':
             if y == True:
                 num_visible += 1
 
-    print(num_visible)
+    return num_visible
+
+
+if __name__ == '__main__':
+    a = [[int(l) for l in line.strip()] for line in open("8_input.txt").readlines()]
+    b = np.array(a, dtype=int)
+
+    num_visible = part1(b)
+    print("part 1:", num_visible)
+
+    # hold the scenic score of each tree
+    c = np.zeros_like(b)
+    h, w = b.shape
+
+    # row, col is tree getting score for
+    for row in range(h):
+        for col in range(w):
+            score_down, score_up, score_right, score_left = 0, 0, 0, 0
+            current = b[row, col]
+            # look down
+            for i in range(row + 1, h, 1):
+                score_down += 1
+                if b[i, col] >= current:
+                    break
+            # look up
+            for i in range(row - 1, -1, -1):
+                score_up += 1
+                if b[i, col] >= current:
+                    break
+            # look right
+            for i in range(col + 1, w, 1):
+                score_right += 1
+                if b[row, i] >= current:
+                    break
+            # look left
+            for i in range(col - 1, -1, -1):
+                score_left += 1
+                if b[row, i] >= current:
+                    break
+
+            c[row, col] = score_down * score_up * score_right * score_left
+
+    print("Part 2:", np.max(c))
