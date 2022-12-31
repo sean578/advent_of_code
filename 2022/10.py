d3e94049402e3550da89a243e12c784599e5ad8d
Sean Donnellan <sean.donnellan@blueprism.com>
2022-12-31 23:23:12 +0000

    Part 2

--- 2022/10.py
+++ 2022/10.py
@@ -1,5 +1,8 @@
-"""
-"""
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
     commands = [line.strip().split() for line in open("10_input.txt").readlines()]
@@ -20,4 +23,15 @@ if __name__ == '__main__':
     for i in range(20, len(reg_vals), 40):
         answer += i * reg_vals[i-1]
 
-    print(answer)
+    print(f"Answer part 1: {answer}")
+
+    screen_vals = []
+    for i, s in enumerate(reg_vals[:-1]):
+        if abs((i % 40) - s) <= 1:
+            screen_vals.append(1)
+        else:
+            screen_vals.append(0)
+
+    a = np.array(screen_vals).reshape((6, 40))
+    plt.imshow(a)
+    plt.show()
