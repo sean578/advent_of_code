3fee6eff5844f0de86e57bfdd1f2fd9c75e35a25
Sean Donnellan <sean.donnellan@blueprism.com>
2023-12-27 21:18:29 +0000

    2023 day 14 part 2

--- 2023/14.py
+++ 2023/14.py
@@ -1,3 +1,8 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+
 def print_grid(grid):
     print()
     for line in grid:
@@ -16,18 +21,39 @@ def add_border(grid):
     return grid
 
 
-def move_rock(grid, y, x):
-    # todo: sort out boundaries
+def move_rock(grid, y, x, direction):
     assert grid[y][x] == "O"
 
     can_move = True
     while can_move:
-        if grid[y - 1][x] == ".":
-            grid[y - 1][x] = "O"
-            grid[y][x] = "."
-            y -= 1
-        else:
-            can_move = False
+        if direction == "north":
+            if grid[y - 1][x] == ".":
+                grid[y - 1][x] = "O"
+                grid[y][x] = "."
+                y -= 1
+            else:
+                can_move = False
+        elif direction == "south":
+            if grid[y + 1][x] == ".":
+                grid[y + 1][x] = "O"
+                grid[y][x] = "."
+                y += 1
+            else:
+                can_move = False
+        elif direction == "west":
+            if grid[y][x - 1] == ".":
+                grid[y][x - 1] = "O"
+                grid[y][x] = "."
+                x -= 1
+            else:
+                can_move = False
+        elif direction == "east":
+            if grid[y][x + 1] == ".":
+                grid[y][x + 1] = "O"
+                grid[y][x] = "."
+                x += 1
+            else:
+                can_move = False
 
     return grid
 
@@ -44,15 +70,53 @@ def get_load(grid):
 
 if __name__ == '__main__':
     grid = [list(line.strip("\n")) for line in open("14_input.txt").readlines()]
-    # print_grid(grid)
     grid = add_border(grid)
-    # print_grid(grid)
 
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            if grid[y][x] == "O":
-                grid = move_rock(grid, y, x)
+    y_max = len(grid)
+    x_max = len(grid[0])
 
-    # print_grid(grid)
-    load = get_load(grid)
-    print(load)
+    cycles, loads = [], []
+    for cycle in range(300):
+
+        # Move north
+        for y in range(y_max):
+            for x in range(x_max):
+                if grid[y][x] == "O":
+                    grid = move_rock(grid, y, x, "north")
+
+        # Move west
+        for x in range(x_max):
+            for y in range(y_max):
+                if grid[y][x] == "O":
+                    grid = move_rock(grid, y, x, "west")
+
+        # Move south
+        for y in range(y_max-1, -1, -1):
+            for x in range(x_max):
+                if grid[y][x] == "O":
+                    grid = move_rock(grid, y, x, "south")
+
+        # Move east
+        for x in range(x_max-1, -1, -1):
+            for y in range(y_max):
+                if grid[y][x] == "O":
+                    grid = move_rock(grid, y, x, "east")
+
+        load = get_load(grid)
+        cycles.append(cycle)
+        loads.append(load)
+
+    plt.scatter(cycles, loads, s=2)
+    plt.show()
+
+    # debug:
+    # offset = 10
+    # time_period = 7
+
+    # input:
+    # Found by looking at plot (offset is arb as long as larger than time for oscillation to begin)
+    offset = 150
+    time_period = 42
+
+    index = (1000000000 - offset) % time_period
+    print(index-1, loads[offset + index - 1])
