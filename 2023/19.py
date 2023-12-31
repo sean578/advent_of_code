c53f46f12bd9ede3fd4eda3ea416627821594516
Sean Donnellan <sean.donnellan@blueprism.com>
2023-12-31 22:57:29 +0000

    2023 day 19 - part 2

--- 2023/19.py
+++ 2023/19.py
@@ -1,3 +1,7 @@
+import copy
+from collections import defaultdict
+
+
 def process_input(lines):
 
     processes_raw = []
@@ -114,10 +118,110 @@ def get_total_part_1(accepted_parts):
     return total
 
 
+def get_graph(processes):
+    graph = defaultdict(list)
+    for name, rules in processes.items():
+        for i in range(len(rules)):
+            # i is the rule we are constructing (need all rules up to that index)
+            new_rules = []
+            if i == 0:
+                # append new rule as is
+                new_rule = {key: value for key, value in rules[i].items() if
+                            key != "rule_destination"}
+                graph[name].append((rules[i]["rule_destination"], new_rule))
+            else:
+                # append the rule at the current index and the opposite of the other rules
+                # Rules we don't want to be true must be opposite
+                for j in range(0, i):
+                    new_rule = {key: value for key, value in rules[j].items() if
+                                key != "rule_destination"}
+                    if new_rule["rule_operation"] == ">":
+                        new_rule["rule_operation"] = "<="
+                    elif new_rule["rule_operation"] == "<":
+                        new_rule["rule_operation"] = ">="
+                    else:
+                        assert False
+                    new_rules.append(new_rule)
+                # Rule we want to be true is the same
+                new_rule = {key: value for key, value in rules[i].items() if
+                            key != "rule_destination"}
+                if new_rule["rule_part"] is not None:
+                    new_rules.append(new_rule)
+                graph[name].append((rules[i]["rule_destination"], new_rules))
+    return graph
+
+
+def flatten(container):
+    for i in container:
+        if isinstance(i, (list, tuple)):
+            for j in flatten(i):
+                yield j
+        else:
+            yield i
+
+
 if __name__ == '__main__':
     lines = [line.strip() for line in open("19_input.txt").readlines()]
-    processes, parts = process_input(lines)
+    processes, _ = process_input(lines)
+
+    path = []
+    target_node_name = "A"
+    graph = get_graph(processes)
+    graph["R"] = []
+    graph["A"] = []
+    rules_for_paths = []
+
+    def dfs(node_name, rules):
+
+        # If we are at the target node we have found a path - print it
+        if node_name == target_node_name:
+            rules_for_paths.append(copy.deepcopy(rules))
+        else:
+            path.append(node_name)
+
+            # If not then explore neighbours - collecting rules
+            for neighbours in graph[node_name]:
+                neighbour_node_name, r = neighbours
+                rules.append(r)
+                dfs(neighbour_node_name, rules)
+                rules.pop()
+
+            path.pop()
 
     # part 1
-    accepted_parts = part_1(processes, parts)
-    print(get_total_part_1(accepted_parts))
+    # accepted_parts = part_1(processes, parts)
+    # print(get_total_part_1(accepted_parts))
+
+    dfs(node_name="in", rules=[])
+
+    total_combinations = 0
+    for rules_for_path in rules_for_paths:
+        rules_flat = list(flatten(rules_for_path))
+        # Start off with full ranges
+        allowed_ranges = {
+            "x": set(range(1, 4000 + 1)),
+            "m": set(range(1, 4000 + 1)),
+            "a": set(range(1, 4000 + 1)),
+            "s": set(range(1, 4000 + 1))
+        }
+        # For each rule in the path, remove the range not allowed
+        for rule in rules_flat:
+            if rule["rule_operation"] == "<":
+                not_allowed = set(range(rule["rule_val"], 4000 + 1))
+                allowed_ranges[rule["rule_part"]] -= not_allowed
+            elif rule["rule_operation"] == "<=":
+                not_allowed = set(range(rule["rule_val"] + 1, 4000 + 1))
+                allowed_ranges[rule["rule_part"]] -= not_allowed
+            elif rule["rule_operation"] == ">":
+                not_allowed = set(range(1, rule["rule_val"] + 1))
+                allowed_ranges[rule["rule_part"]] -= not_allowed
+            elif rule["rule_operation"] == ">=":
+                not_allowed = set(range(1, rule["rule_val"]))
+                allowed_ranges[rule["rule_part"]] -= not_allowed
+
+        # Can assume combinations from each path are independent...
+        total = 1
+        for key, value in allowed_ranges.items():
+            total *= len(value)
+        total_combinations += total
+    print(total_combinations)
